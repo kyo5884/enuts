8bbf80e82df125567b23f837cead919653d519b9
kyo5884 <kyo5884.public@gmail.com>
2018-01-29 20:56:00 +0900

    automatic zip!

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+PKG_VERSION=`node -pe 'require("./package.json").version'`
+
 electron-packager . enuts --platform=win32 --arch=ia32 --electron-version=1.7.11 --overwrite --icon=img/enuts_icon.ico
 cp -r enuts-win32-ia32/ "/Users/kyo5884/VirtualBox VMs/share-folder/enuts-win32-ia32"
 
@@ -9,3 +11,8 @@ cp -r enuts-linux-ia32/ "/Users/kyo5884/VirtualBox VMs/share-folder/enuts-linux-
 electron-packager . enuts --platform=linux --arch=x64 --electron-version=1.7.11 --overwrite --icon=img/enuts_icon.ico
 
 electron-packager . enuts --platform=darwin --arch=x64 --electron-version=1.7.11 --overwrite --icon=img/enuts_icon.icns
+
+zip -r ~/Desktop/enuts-${PKG_VERSION}-linux.zip enuts-linux-ia32/
+zip -r ~/Desktop/enuts-${PKG_VERSION}-linux-x64.zip enuts-linux-x64/
+zip -r ~/Desktop/enuts-${PKG_VERSION}-win32.zip enuts-win32-ia32/
+zip -r ~/Desktop/enuts-${PKG_VERSION}-darwin-x64.zip enuts-darwin-x64/
