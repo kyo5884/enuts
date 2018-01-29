58b7230ed8c9a0e73172d1561eb7d9ba1f38ea9f
kyo5884 <kyo5884.public@gmail.com>
2018-01-29 15:11:35 +0900

    updated electron

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-electron-packager . enuts --platform=win32 --arch=ia32 --version=1.4.13 --overwrite --icon=img/enuts_icon.ico
+electron-packager . enuts --platform=win32 --arch=ia32 --electron-version=1.6.14 --overwrite --icon=img/enuts_icon.ico
 cp -r enuts-win32-ia32/ "/Users/kyo5884/VirtualBox VMs/share-folder/enuts-win32-ia32"
 
-electron-packager . enuts --platform=linux --arch=ia32 --version=1.4.13 --overwrite --icon=img/enuts_icon.ico
+electron-packager . enuts --platform=linux --arch=ia32 --electron-version=1.6.14 --overwrite --icon=img/enuts_icon.ico
 cp -r enuts-linux-ia32/ "/Users/kyo5884/VirtualBox VMs/share-folder/enuts-linux-ia32"
 
-electron-packager . enuts --platform=linux --arch=x64 --version=1.4.13 --overwrite --icon=img/enuts_icon.ico
+electron-packager . enuts --platform=linux --arch=x64 --electron-version=1.6.14 --overwrite --icon=img/enuts_icon.ico
 
-electron-packager . enuts --platform=darwin --arch=x64 --version=1.4.13 --overwrite --icon=img/enuts_icon.icns
+electron-packager . enuts --platform=darwin --arch=x64 --electron-version=1.6.14 --overwrite --icon=img/enuts_icon.icns
